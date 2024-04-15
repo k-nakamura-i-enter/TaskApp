68324b3fb421f7d8b454db12cd2a3228c7077868
nakanakamuramura <k-nakamura@i-enter.co.jp>
2024-04-15 16:40:09 +0900

    まとめの前段階（要件見直し中）

---
+++ TaskApp/Task.swift
@@ -0,0 +1,26 @@
+//
+//  Task.swift
+//  TaskApp
+//
+//  Created by 中村 行汰 on 2024/04/15.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @Persisted(primaryKey: true) var id: ObjectId
+
+    // タイトル
+    @Persisted var title = ""
+
+    // 内容
+    @Persisted var contents = ""
+
+    // 日時
+    @Persisted var date = Date()
+    
+    //カテゴリ
+    @Persisted var category = ""
+
+}
--- TaskApp/ViewController.swift
+++ TaskApp/ViewController.swift
@@ -6,9 +6,20 @@
 //
 
 import UIKit
+import RealmSwift
+import UserNotifications
 
 class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
+    
+    // Realmインスタンスを取得する
+    let realm = try! Realm()
+
+    // DB内のタスクが格納されるリスト。
+    // 日付の近い順でソート：昇順
+    // 以降内容をアップデートするとリスト内は自動的に更新される。
+    var taskArray = try! Realm().objects(Task.self).sorted(byKeyPath: "date", ascending: true)
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,16 +29,44 @@ class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSourc
         tableView.dataSource = self
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+    // segue で画面遷移する時に呼ばれる
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        let inputViewController:InputViewController = segue.destination as! InputViewController
+
+        if segue.identifier == "cellSegue" {
+            let indexPath = self.tableView.indexPathForSelectedRow
+            inputViewController.task = taskArray[indexPath!.row]
+        } else {
+            inputViewController.task = Task()
+        }
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return taskArray.count
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        // Cellに値を設定する
+        let task = taskArray[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = task.title
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm"
+        let dateString:String = formatter.string(from: task.date)
+        content.secondaryText = dateString
+        cell.contentConfiguration = content
+        
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        performSegue(withIdentifier: "cellSegue",sender: nil) // ←追加する
+        performSegue(withIdentifier: "cellSegue",sender: nil)
     }
     
     func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath)-> UITableViewCell.EditingStyle {
@@ -35,6 +74,29 @@ class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSourc
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // 削除するタスクを取得する
+            let task = self.taskArray[indexPath.row]
+
+            // ローカル通知をキャンセルする
+            let center = UNUserNotificationCenter.current()
+            center.removePendingNotificationRequests(withIdentifiers: [String(task.id.stringValue)])
+
+            // データベースから削除する
+            try! realm.write {
+                self.realm.delete(task)
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+
+            // 未通知のローカル通知一覧をログ出力
+            center.getPendingNotificationRequests { (requests: [UNNotificationRequest]) in
+                for request in requests {
+                    print("/---------------")
+                    print(request)
+                    print("---------------/")
+                }
+            }
+        }
     }
 }
 
